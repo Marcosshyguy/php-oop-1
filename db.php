244fff5eaf7427aacc941654522eef17ce1d015c
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-12-11 16:01:35 +0100

    add some movies

--- db.php
+++ db.php
@@ -2,8 +2,8 @@
 require_once __DIR__  . "/MODEL/Movie.php";
 
 $movies = [
-
     $nightmare_before_christmas = new Movie('Nightmare Before Christmas', new Genre('fantasy', 'horror'), 'Tim Burton', 'October 13, 1993'),
     $weekend_at_bernie = new Movie("Weekend at Bernie's", new Genre('comedy'), 'Ted KotCheff', 'July 5, 1989', 'Movie with a dead man and two guys that pretend to make him look alive'),
-
+    $back_to_the_future = new Movie("Back to the Future", new Genre('sci-fi', 'comedy'), 'Robert Zemekis', 'July 3, 1985'),
+    $citizen_kane = new Movie('Citizen Kane!', new Genre('Drama'), 'Orson Wells', 'September 5, 1941')
 ];
